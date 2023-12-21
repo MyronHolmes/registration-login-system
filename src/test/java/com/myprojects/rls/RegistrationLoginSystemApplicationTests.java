13e27f5b8fcd5816cca57d9f9aef582a091d0276
Myron Holmes <1dequanh@gmail.com>
2023-12-21 16:18:43 -0500

    initial commit

---
+++ src/test/java/com/myprojects/rls/RegistrationLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.rls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
